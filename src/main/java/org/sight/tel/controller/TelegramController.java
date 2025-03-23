c27fc75a1b3910972da12ed612b7ed84d5c1dc88
Lee Joo Won <first0627@hanmail.net>
2025-03-23 16:43:57 +0900

    13

--- src/main/java/org/sight/tel/controller/TelegramController.java
+++ src/main/java/org/sight/tel/controller/TelegramController.java
@@ -5,9 +5,7 @@ import org.sight.tel.entity.SubscriberHistory;
 import org.sight.tel.service.TelegramService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "https://tele-front-git-main-lee-joo-wons-projects.vercel.app")
+@CrossOrigin(origins = {"http://localhost:5173", "https://tele-front-xi.vercel.app"})
 @RestController
 @RequestMapping("/api/telegram")
 public class TelegramController {
